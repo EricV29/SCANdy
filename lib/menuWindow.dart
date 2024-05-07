bc9806ac253816d5b32094c0fbff7163c43c69ff
EricVilleda <ericjared29@gmail.com>
2024-05-07 13:09:11 -0600

    addSwitchFunction

--- lib/menuWindow.dart
+++ lib/menuWindow.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class menuWindow extends StatefulWidget {
   @override
@@ -6,51 +7,67 @@ class menuWindow extends StatefulWidget {
 }
 
 class _menuWindowState extends State<menuWindow> {
-  int _selectedIndex = 0;
+  int? _selectedIndex = 0;
+
+  void miFuncion(index) {
+    print('¡Hola desde miFuncion!');
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget buildToggleSwitch() {
+    return ToggleSwitch(
+      minWidth: 115.0,
+      cornerRadius: 20.0,
+      activeBgColors: [[Color(0xffFF6996)], [Color(0xffFF6996)]],
+      activeFgColor: Colors.white,
+      inactiveBgColor: Colors.white,
+      inactiveFgColor: Colors.black,
+      initialLabelIndex: _selectedIndex,
+      totalSwitches: 2,
+      borderColor: [Color(0xff000000)],
+      borderWidth: 1.0,
+      labels: ['Escaner', 'Inventario'],
+      customTextStyles: [
+        TextStyle(fontFamily: 'Lemonr', fontSize: 15),
+        TextStyle(fontFamily: 'Lemonr', fontSize: 15),
+      ],
+      radiusStyle: true,
+      onToggle: (index) {
+        miFuncion(index);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(0),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(1.0),
+          child: Container(
+            color: Color(0xffDEDEDE),
+            height: 1.0,
           ),
-          side: BorderSide(color: Color(0xffDEDEDE), width: 1),
         ),
-        actions: [
-          ToggleButtons(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('Escaner'),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('Inventario'),
-              ),
-            ],
-            onPressed: (int index) {
-              setState(() {
-                _selectedIndex = index;
-              });
-            },
-            isSelected: [_selectedIndex == 0, _selectedIndex == 1],
-            color: Colors.black,
-            selectedColor: Colors.white,
-            fillColor: Colors.pink,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Image.asset('assets/Logotxt.png'),
-          ),
-        ],
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            buildToggleSwitch(),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Image.asset('assets/Logotxt.png'),
+            ),
+          ],
+        ),
       ),
       body: _selectedIndex == 0 ? PantallaEscaner() : PantallaInventario(),
     );
   }
 }
+
 
 class PantallaEscaner extends StatelessWidget {
   @override
